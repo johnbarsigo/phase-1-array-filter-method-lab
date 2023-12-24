4f9cdae29ed45a5bfdddf3235a2991922be3ebd0
johnbarsigo <john.barsigo@student.moringaschool.com>
2023-12-24 16:47:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution here
+const driversArray = ["Bobby", "Sammy", "Sally"];
+
+//NO.1
+
+function findMatching(drivers, match) {
+    return drivers.filter(driver => driver.toLowerCase() === match.toLowerCase());
+  }
+const matchFind = findMatching(driversArray, "sammy");
+//console.log(matchFind);
+
+  //NO.2
+
+function fuzzyMatch(drivers, match) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(match.toLowerCase()));
+}
+const matchingDrivers = fuzzyMatch(driversArray, "B");
+//console.log(matchingDrivers);
+
+//NO.3
+
+function matchName(drivers, match) {
+    return drivers.filter(driver => driver.name.toLowerCase() === match.toLowerCase());
+  }
